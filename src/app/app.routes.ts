3787c099402f3a1707849d6bd397b895b50a2c03
Rohitkashiwale <rohitkashiwale19@gmail.com>
2026-03-30 16:49:07 +0530

    Creating the correct folder structure and and creating the rountng for all the component and designing the navbar and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",loadComponent: ()=> import('./features/home/home').then((m)=>m.Home)},
+    {
+        path:"shop",
+        loadComponent:()=> import('./features/shop/shop').then((m)=>m.Shop)
+    },
+    {
+        path:"cart",
+        loadComponent:()=> import('./features/cart/cart').then((m)=>m.Cart)
+    },
+  
+    {
+        path:"user",
+        loadComponent: ()=> import('./features/user/user').then((m)=>m.User)
+    },
+    {
+        path:"wishlist",
+        loadComponent: ()=>import('./features/wishlist/wishlist').then((m)=>m.Wishlist)
+    }
+];
